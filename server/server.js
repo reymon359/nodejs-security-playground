9f04f3da00cb051871cc09ae0554f329f0b809ae
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-31 06:54:27 +0200

    feat(server): Use the logger Morgan on the server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 var winston = require('winston');
 
 const bodyParser = require('body-parser');
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(bodyParser.json())
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/secret', (req, res) => {
-  const { userInput } = req.body;
-  console.log(userInput);
-  if (userInput) {
-    winston.log('info', 'user input: ' + userInput);
-    res.status(200).json('success');
-  } else {
-    winston.error('This guy is messing with us:' + userInput);
-    res.status(400).json('incorrect submission')
-  }
-})
+	const { userInput } = req.body;
+	console.log(userInput);
+	if (userInput) {
+		winston.log('info', 'user input: ' + userInput);
+		res.status(200).json('success');
+	} else {
+		winston.error('This guy is messing with us:' + userInput);
+		res.status(400).json('incorrect submission');
+	}
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
