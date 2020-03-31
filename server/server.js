d90fc5adc6a46af1dd1538e802453b9b7bfcd566
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-31 07:31:54 +0200

    feat(server): Add Content-Security-Policy to headers

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ app.use(helmet());
 app.use(bodyParser.json());
 // app.use(morgan('combined'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+	res.set({
+		'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+	});
+	res.send('Hello World!');
+});
 
 app.post('/secret', (req, res) => {
 	const { userInput } = req.body;
